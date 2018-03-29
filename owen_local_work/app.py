bc2bc31b826b96f2cce8bf64ab9134a2d90620e6
Owen Lawrence <owen@calvisitor-10-105-167-38.calvisitor.1918.berkeley.edu>
2018-03-28 20:46:26 -0700

    dropdown selection list

--- owen_local_work/app.py
+++ owen_local_work/app.py
@@ -36,15 +36,24 @@ session = Session(engine)
 # Homepage
 @app.route('/')
 def index():
-    data = pd.read_csv('data.csv')
     return render_template('index.html')
 
 # Pie Chart
 
 # Bar Chart
+@app.route("/countries")
+def country():
+    results = session.query(Hacker.CountryNumeric2).distinct()#.\
+        # func.distinct(Hacker.CountryNumeric2).all()
+
+    countries = [row[0] for row in results]
+
+    return jsonify(countries)
+
+
 @app.route("/bar")
 def ages():
-    results = session.query(Hacker.q3Gender1, Hacker.q1AgeBeginCoding, Hacker.CountryNumeric2).all()#.\
+    results = session.query(Hacker.RespondentID, Hacker.q1AgeBeginCoding, Hacker.CountryNumeric2).all()#.\
         # func.distinct(Names.id).all()
 
     ids = [row[0] for row in results]
