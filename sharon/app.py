a961da51d1264f3043235e845fdf75c0b4c92fc4
sharonsu94 <sharonsu103@gmail.com>
2018-03-28 21:14:49 -0700

    app.py

--- sharon/app.py
+++ sharon/app.py
@@ -38,22 +38,28 @@ def index():
     return render_template('index.html')
 
 # Pie Chart
-@app.route("/pie")
-def learning():
+@app.route("/pie/<country>")
+def learning(country):
     results = session.query(Hacker.RespondentID, Hacker.CountryNumeric2, Hacker.q6LearnCodeUni1, Hacker.q6LearnCodeSelfTaught1, Hacker.q6LearnCodeAccelTrain1, 
-    Hacker.q6LearnCodeDontKnowHowToYet1, Hacker.q6LearnCodeOther1).all()
+    Hacker.q6LearnCodeDontKnowHowToYet1, Hacker.q6LearnCodeOther1).filter(Hacker.CountryNumeric2 == country).all()
 
     ids = [row[0] for row in results]
     countries = [row[1] for row in results]
     uni = [row[2] for row in results]
+    total_uni = uni.count(1)
     self_taught = [row[3] for row in results]
+    total_self = self_taught.count(1)
     accelerated = [row[4] for row in results]
+    total_accelerated = accelerated.count(1)
     dont_know = [row[5] for row in results]
+    total_dont = dont_know.count(1)
     other = [row[6] for row in results]
+    total_other = other.count(1)
 
-    return jsonify([{'respondent_ids': ids, 'countries': countries, 'uni': uni, 'self_taught': self_taught, 
-    'accelerated': accelerated, "dont know": dont_know, "other": other }])
 
+    return jsonify([{'uni': str(total_uni), 'self_taught': str(total_self),'accelerated': str(total_accelerated), 
+    "dont know": str(total_dont), "other": str(total_other)}])
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
